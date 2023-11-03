9de1206d8b0cc4d6bdc3d4d94aba54ebb00ac312
harry <harrymac1972@gmail.com>
2023-11-03 01:52:26 -0600

    tidied up the code to end branch

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -97,30 +97,16 @@ class Row {
     this.focusText = 0;
   }
   
-  createListenerBtn(miliTm,timeBtn) {
-    timeBtn.on('click', function() {
-      var keyVal = "id-" + miliTm;
-      var textVal = storage.getTextFromTextArea(miliTm);
-      storage.setKeyValue(keyVal,textVal);
-    });
-  }
-  
-  createListenerText(miliTm,textArea) {
-    textArea.on('click', function() {
-      this.focusText = miliTm;
-    });
-  }
-  
-  createTimeRows() {
+  createAll() {
     var mainCon = $("#main-con");
     mainCon.attr("class","container-fluid px-5");
     for (var militaryTime=9; militaryTime<18; militaryTime++) {
       var miliTm = militaryTime;
-      this.createTimeRowOuterDiv(miliTm);
+      this.createOuterDiv(miliTm);
     }
   }
 
-  createTimeRowOuterDiv(miliTm) {
+  createOuterDiv(miliTm) {
     var rowDiv = $("<div>");
     var hourId = "hour-" + miliTm;
     rowDiv.attr("id",hourId);
@@ -128,10 +114,10 @@ class Row {
     outClassStr += clock.getTimeRowTense(miliTm);
     rowDiv.attr("class",outClassStr);
     $("#main-con").append(rowDiv);
-    this.createTimeRowTimeDiv(miliTm,rowDiv);
+    this.createTimeDiv(miliTm,rowDiv);
   }
 
-  createTimeRowTimeDiv(miliTm,rowDiv) {
+  createTimeDiv(miliTm,rowDiv) {
     var timeDiv = $("<div>");
     var time = "in-time-" + miliTm;
     timeDiv.attr("id",time);
@@ -139,32 +125,39 @@ class Row {
     timeDiv.attr("class",timeClassStr);
     timeDiv.text(clock.getAmPmTime(miliTm));
     $(rowDiv).append(timeDiv);
-    this.createTimeRowTextArea(miliTm,rowDiv);
+    this.createTextArea(miliTm,rowDiv);
   }
 
-  createTimeRowTextArea(miliTm,rowDiv) {
+  createTextArea(miliTm,rowDiv) {
     var textArea = $("<textarea>");
     var textId = "text-" + miliTm;
     textArea.attr("id",textId);
     textArea.attr("class","col-8 col-md-10 description");
     textArea.attr("rows",3);
     $(rowDiv).append(textArea);
-    this.createListenerText(miliTm,textArea);
-    this.createTimeRowBtn(miliTm,rowDiv);
+    this.createBtn(miliTm,rowDiv);
   }
 
-  createTimeRowBtn(miliTm,rowDiv) {
+  createBtn(miliTm,rowDiv) {
     var timeBtn = $("<btn>");
     timeBtn.attr("class","btn saveBtn col-2 col-md-1");
     timeBtn.attr("aria-label","save");
     $(rowDiv).append(timeBtn);
-    this.createListenerBtn(miliTm,timeBtn);
+    this.makeListenerBtn(miliTm,timeBtn);
     var italicStuff = $("<i>");
     italicStuff.attr("class","fas fa-save");
     italicStuff.attr("aria-hidden","true");
     $(timeBtn).append(italicStuff);
   }
 
+  makeListenerBtn(miliTm,timeBtn) {
+    timeBtn.on('click', function() {
+      var keyVal = "id-" + miliTm;
+      var textVal = storage.getTextFromTextArea(miliTm);
+      storage.setKeyValue(keyVal,textVal);
+    });
+  }
+  
   updateAllFromStorage() {
     for (var militaryTime=9; militaryTime<18; militaryTime++) {
       var miliTm = militaryTime;
@@ -211,7 +204,7 @@ storage = new Storage();
 
 function main() {
   head.initHead();
-  row.createTimeRows();
+  row.createAll();
   row.updateAllFromStorage();
 }
 
@@ -219,29 +212,3 @@ main();
 
 
 //#endregion
-
-
-// HARRY:
-// NOTE: put the listener on the div with ID as event bubbling will occur
-
-
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
